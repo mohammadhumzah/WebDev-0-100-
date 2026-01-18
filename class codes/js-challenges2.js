b986cc9465ab108636b04f304fcae304963e3c61
mohammadhumzah <halwaihamza771@gmail.com>
2026-01-19 00:29:00 +0530

    More challenges

--- class codes/js-challenges2.js
+++ class codes/js-challenges2.js
@@ -10,13 +10,72 @@
 
 // 
 
-function sumofnumbers(end){
-    sum = 0
-    for(let i=0; i<=end; i++){
-        sum+=i
-    }
-    return sum
-}
+// function sumofnumbers(end){
+//     sum = 0
+//     for(let i=0; i<=end; i++){
+//         sum+=i
+//     }
+//     return sum
+// }
 
-console.log(sumofnumbers(100))
+// console.log(sumofnumbers(100))
+
+
+
+
+// let arr = [2,4,12,13,8,7]
+
+// for (let i=0; i<arr.length; i++){
+//     if (arr[i]<10){
+//         continue
+//     }
+//     else{
+//         console.log(arr[i])
+//     }
+// } 
+
+
+
+
+// let word = "abababb"
+// let count=0
+// for(let i=0;i<word.length;i++){
+//     if(word[i]!="b"){
+//         continue
+//     }
+//     else{
+//         count+=1
+//     }
+    
+// }
+// console.log(count)
+
+
+// let arr = [2,12.15,718,19]
+// let max = arr[0]
+
+
+// for (let i=1; i < arr.length; i++){
+//     if(arr[i]>max){
+//         max=arr[i]
+//     }
+    
+// }
+// console.log(max)
+ 
+
+// let arr = [1,3,4,2,1213,15,18,6473,242]
+// let even =[]
+// let odd = []
+// for (let i=0; i < arr.length; i++){
+//     if (arr[i]%2===0){
+//         even.push(arr[i])
+//     }
+//     else{
+//         odd.push(arr[i])
+//     }
+// }
+// console.log(even,odd)
+
+
 
