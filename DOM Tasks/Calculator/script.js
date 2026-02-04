5afd012cd3d04360ea0e26a7b75ae4428e1bcbc7
mohammadhumzah <halwaihamza771@gmail.com>
2026-02-04 23:53:31 +0530

    Logic for calc

---
+++ DOM Tasks/Calculator/script.js
@@ -0,0 +1,25 @@
+// Create var and find the keypad and screen element with help of query selector
+const keypad = document.querySelector('.keypad')
+const screen = document.querySelector('.screen input')
+
+keypad.addEventListener('click', (even) => {
+
+    // Check if button was clicked or not
+    if(even.target.tagName === 'BUTTON'){
+        // Use .target to know which exact element triggered event, .dataset.type to get custom dataset of triggerer
+        const buttonType = even.target.dataset.type
+        let buttonValue = even.target.textContent
+
+        if(buttonValue === '✖️'){
+            buttonValue = '*'
+        }
+
+        // Handle based on type of input - INSIDE HERE!
+        if(buttonType === 'number' || buttonType === 'operator'){
+            screen.value += buttonValue
+        }
+        else if (buttonType === 'equal') {
+            screen.value = eval(screen.value)
+        }
+    }
+})
