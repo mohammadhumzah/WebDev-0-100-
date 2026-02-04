0e39a6aa6154124eebbdbc2f8016854c3c1557ac
mohammadhumzah <halwaihamza771@gmail.com>
2026-02-04 18:46:24 +0530

    Added BMI calculation to result

--- DOM Tasks/BMI Index/script.js
+++ DOM Tasks/BMI Index/script.js
@@ -5,24 +5,24 @@ const finalResult = document.getElementById('results')
 
 submissionButton.addEventListener('click', () => {
 
-    const valueWeight = Number(weightSubmit.value)          // Number is used because input field takes numbers as strings to be converted
+    const valueWeight = Number(weightSubmit.value) // Number used coz input field takes numbers as strings to be converted
     const valueHeight = Number(heightSubmit.value) / 100   // Convert cm to meters
     const bmi = (valueWeight / valueHeight ** 2) 
 
     if( bmi >= 0 && bmi < 16){
-        finalResult.innerText = 'Underweight'
+        finalResult.innerText = `Underweight - Your BMI is ${bmi.toFixed(2)}`
     }
     else if(bmi >= 16 && bmi < 18.5){
-        finalResult.innerText = 'Mild Thinness'
+        finalResult.innerText = `Mild Thinness - Your BMI is ${bmi.toFixed(2)}`
     }
     else if(bmi >= 18.5 && bmi < 25 ){
-        finalResult.innerText = 'Normal'
+        finalResult.innerText = `Normal - Your BMI is ${bmi.toFixed(2)}`
     }
     else if(bmi >= 25 && bmi < 30){
-        finalResult.innerText = 'Overweight'
+        finalResult.innerText = `Overweight - Your BMI is ${bmi.toFixed(2)}`
     }
     else{
-        finalResult.innerText = 'Obese'
+        finalResult.innerText = `Obese - Your BMI is ${bmi.toFixed(2)}`
     }
 
 
